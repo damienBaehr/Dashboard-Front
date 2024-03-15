9b64fad751e6f480302c01472cf35ebc5116f74d
Emk0oo <emir.j2002@gmail.com>
2024-03-15 23:45:01 +0100

    init push

---
+++ dashboard--front/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold">AD LAURENT</h1>
+      <div>
+        <input type="text" className="border-2" />
+        <input type="text" className="border-2" />
+        <button className="bg-blue-500 text-white">Submit</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
