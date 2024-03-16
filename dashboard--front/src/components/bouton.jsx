6a2a5ded9ceb745af63ae09bd90603a4bed0f810
Emk0oo <emir.j2002@gmail.com>
2024-03-16 12:14:52 +0100

    [ADD] components folder

---
+++ dashboard--front/src/components/bouton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const bouton = () => {
+  return (
+    <button
+                className="bg-blue-500 text-white rounded p-1 "
+                onClick={envoyer}
+              >
+                Login
+              </button>
+  )
+}
+
+export default bouton
--- dashboard--front/src/main.tsx
+++ dashboard--front/src/main.tsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>,
 )
